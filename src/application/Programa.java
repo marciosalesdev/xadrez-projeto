e39fdf941e1432bebb83449b9de4f8a054bb0541
Developer <marcioserrasales2014@gmail.com>
2024-09-25 14:36:54 -0300

    criando camada de xadrez e impressao do tabuleiro

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,12 +1,14 @@
 package application;
 
-import boardgame.Board;
+import chess.ChessMatch;
 
 
 public class Programa {
 
 	public static void main(String[] args) {
-		Board board = new Board(8, 8);
+		ChessMatch chesMatch = new ChessMatch();
+		UI.printBoard(chesMatch.getPieces());
 		
 
-}}
+	}	
+}
